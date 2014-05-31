22e8109f48f7aef746195e9058fa0e58e3fc023f
Gaelan D'costa <gdcosta@gmail.com>
2014-05-31 16:41:06 -0400

    We can slurp content from a submitted URL.

--- web.rb
+++ web.rb
@@ -1,7 +1,16 @@
 require 'bundler/setup'
 
+require 'open-uri'
+
+require 'json'
 require 'sinatra'
 
 get '/' do
-  "Hello, world"
+  haml(:index, format: :html5)
+end
+
+post '/cases' do
+  decision = open(params[:decisionurl1]).read
+
+  decision
 end
